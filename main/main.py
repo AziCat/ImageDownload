bf5fc86115fe38b081057e793c650c94d789fdf5
AziCat <1003980136@qq.com>
2017-01-18 22:41:06 +0800

    功能改造

--- main/main.py
+++ main/main.py
@@ -5,24 +5,34 @@ import os
 import config
 import threading
 import time
-
-
+import sys
+from time import sleep
+reload(sys)
+sys.setdefaultencoding('utf-8')
     
 def getImageName(position,url):  
     urlArr = url.split(".")
     return  position+"."+urlArr[len(urlArr)-1]
     
-def saveImage(url,name):
+def saveImage(url,name,threadName):
     path = config.downloadPath
     if os.path.exists(path) == False:
         os.mkdir(path)
         print "创建目录"+path
-         
-    conn = urllib2.urlopen(url)  
-    f = open(path+"\\"+name,'wb')  
+    try:
+        print threadName+"开始下载"+name
+        headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+        req = urllib2.Request(url = url,headers = headers)
+        feeddata = urllib2.urlopen(req,timeout=10).read()  
+        f = open(path+"\\"+name,'wb')  
       
-    f.write(conn.read())  
-    f.close()
+        f.write(feeddata)  
+        f.close()   
+    except BaseException:
+        print name+"下载异常，暂停5秒"
+        sleep(5)
+        saveImage(url,name,threadName);
+    
     print "下载完成-->"+path+"\\"+name
 
 def downloadThread(threadName,downLoadList,start,end):
@@ -30,7 +40,7 @@ def downloadThread(threadName,downLoadList,start,end):
     while start<end:
         url = downLoadList[start]
         name = getImageName(str(start), url)
-        saveImage(url, name)
+        saveImage(url, name,threadName)
         start=start+1
     print threadName+"执行完毕"
     
